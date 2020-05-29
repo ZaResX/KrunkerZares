9291359f74b5a9fbf93db39c708c5aa8b0c508b1
ZaResX <66065991+ZaResX@users.noreply.github.com>
2020-05-29 09:25:38 +0300

    Create NameTags.js

---
+++ TamperMonkey/SeeNameTags/NameTags.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         Krunker NameTags
+// @namespace    https://github.com/ZaResX/KrunkerZares/
+// @version      2.4.4
+// @description  HRT leak v SkidLamer Fix v Own Edit
+// @author       ZaRes X
+// @match        *://krunker.io/*
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let initialize = function(data) {
+        let regex = /if\(!\w+\['(\w+)']\)continue/; //Hook
+        let result = regex.exec(data);
+        if (result) {
+            const inView = result[1];
+            const push = Array.prototype.push;
+            Array.prototype.push = function(...args) {
+                push.apply(this, args);
+                if (args[0] instanceof Object && args[0].isPlayer) { //isPlayer is the value. If this get patched change isPlayer to new value to make it work
+                    Object.defineProperty(args[0], inView, {value: true, configurable: false});
+                }
+            }
+        }
+    }
+    //Hook Control
+    const decode = window.TextDecoder.prototype.decode; 
+    window.TextDecoder.prototype.decode = function(...args) {
+        let data = decode.apply(this, args);
+        if (data.length > 1050000) {
+            initialize(data);
+        }
+        return data;
+    }
+})();
+// https://github.com/hrt/KrunkerBypass/blob/master/ArrayHook/esp.js
