b9d483cc2b9415979dd4ef99d37a3612725f4873
[Z] [R] [X] <66065991+ZaResX@users.noreply.github.com>
2020-09-27 15:36:15 +0300

    Update NameTags.js

--- TamperMonkey/SeeNameTags/NameTags.js
+++ TamperMonkey/SeeNameTags/NameTags.js
@@ -1,51 +1,31 @@
 // ==UserScript==
 // @name         Krunker NameTags
-// @namespace    https://skidlamer.github.io/
-// @version      0.2
-// @description  self leak
-// @author       SkidLamer, fixed by chomler
+// @author       ɹaɹoldxa ʇauɹaʇu!#2036
+// @description  More krunker mods here : https://discord.gg/B9CrQ3a
+// @grant        GM.xmlHttpRequest
 // @match        *://krunker.io/*
 // @run-at       document-start
-// @grant        none
 // ==/UserScript==
-
-(function() {
-    'use strict';
-    let inView
-    const initialize = function(data) {
-        let result = /if\(!\w+\['(\w+)']\)continue/.exec(data);
-        if (result) {
-            inView = result[1];
-        }
+(async(f) => {
+  f = await GM.xmlHttpRequest({
+    url : "https://krunker.io/social.html",
+    responseType : "json"
+  });
+  b = await GM.xmlHttpRequest({
+    url : `https://krunker.io/pkg/krunker.${/\w.exports="(\w+)"/.exec(f.responseText)[1]}.vries`,
+    responseType : "arrayBuffer"
+  });
+  c = Array.from(new Uint8Array(b.response));
+  d = 33 ^ c[0];
+  e = c.map((c) => {
+    return String.fromCharCode(c ^ d);
+  }).join("");
+  Object.defineProperty(Object.prototype, /if\(!\w+\['(\w+)']\)continue/.exec(e)[1], {
+    get() {
+      return 0 == this._ || this._;
+    },
+    set(next) {
+      this._ = next;
     }
-    const decode = window.TextDecoder.prototype.decode;
-    const decodeHook = function(...args) {
-        let data = decode.apply(this, args);
-        if (data.length > 1050000) {
-            initialize(data);
-        }
-        return data;
-    }
-    const push = Array.prototype.push;
-    const pushHook = function(...args) {
-        push.apply(this, args);
-        if (inView) {
-            if (args[0] instanceof Object && args[0].isPlayer) {
-                Object.defineProperty(args[0], inView, {value: true, configurable: false});
-            }
-        }
-    }
-    window.Object.defineProperty = new Proxy(Object.defineProperty, {
-        apply(target, thisArg, argArray) {
-            argArray[2].configurable = true;
-            if (argArray[2].get && argArray[2].get.name.startsWith("get_")) {
-                if (argArray[1] === "decode")
-                    argArray[2].get =_=> decodeHook
-                else if (argArray[1] === "cpush")
-                    argArray[2].get =_=> pushHook
-            }
-            return target.apply(thisArg, argArray)
-        }
-    })
+  });
 })();
-//https://discord.com/channels/692606346645733396/701441000400224266/740498576567828492
