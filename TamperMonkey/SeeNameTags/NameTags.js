f4662f62c8a7efaa8f2f338ce677a8ae54af0012
[Z] [R] [X] <66065991+ZaResX@users.noreply.github.com>
2020-11-01 19:44:29 +0200

    Update NameTags.js

--- TamperMonkey/SeeNameTags/NameTags.js
+++ TamperMonkey/SeeNameTags/NameTags.js
@@ -1,31 +1,38 @@
 // ==UserScript==
-// @name         Krunker NameTags
-// @author       ɹaɹoldxa ʇauɹaʇu!#2036
-// @description  More krunker mods here : https://discord.gg/B9CrQ3a
-// @grant        GM.xmlHttpRequest
-// @match        *://krunker.io/*
-// @run-at       document-start
+// @name         Kronk name esp
+// @version      3.2.1
+// @author       github.com/hrt
+// @match        https://krunker.io/*
+// @grant        none
 // ==/UserScript==
-(async(f) => {
-  f = await GM.xmlHttpRequest({
-    url : "https://krunker.io/social.html",
-    responseType : "json"
-  });
-  b = await GM.xmlHttpRequest({
-    url : `https://krunker.io/pkg/krunker.${/\w.exports="(\w+)"/.exec(f.responseText)[1]}.vries`,
-    responseType : "arrayBuffer"
-  });
-  c = Array.from(new Uint8Array(b.response));
-  d = 33 ^ c[0];
-  e = c.map((c) => {
-    return String.fromCharCode(c ^ d);
-  }).join("");
-  Object.defineProperty(Object.prototype, /if\(!\w+\['(\w+)']\)continue/.exec(e)[1], {
-    get() {
-      return 0 == this._ || this._;
-    },
-    set(next) {
-      this._ = next;
+
+(function() {
+    'use strict';
+    function hook_join(wnd) {
+        const array_join = wnd.Array.prototype.join;
+        wnd.Array.prototype.join = new Proxy(array_join, {
+            apply: function(target, _this, _arguments) {
+                var ret = Function.prototype.apply.apply(target, [_this, _arguments]);
+                if (_arguments.length && _arguments[0] == '' && _this.length > 1000) {
+                    var game_js = ret;
+                    game_js = game_js.replace(/(if\(!\w+\['\w+'\]\)conti)nue;/, '/*$1*/'); // esp whilst keeping same script length
+                    return game_js;
+                }
+                return ret;
+            }
+        });
     }
-  });
+
+    const append_child = HTMLBodyElement.prototype.appendChild;
+    HTMLBodyElement.prototype.appendChild = new Proxy(append_child, {
+        apply: function(target, _this, _arguments) {
+            var ret = Function.prototype.apply.apply(target, [_this, _arguments]);
+            if (_arguments.length && _arguments[0].__proto__ == HTMLIFrameElement.prototype) {
+                hook_join(_arguments[0].contentWindow);
+            }
+            return ret;
+        }
+    });
 })();
+
+//https://github.com/hrt/KrunkerBypass#joinhook
